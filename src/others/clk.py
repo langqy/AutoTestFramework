a30a0a6c0db9d437ff380a8ce5c95ab6905369c6
huilansame <396214358@qq.com>
2016-09-09 23:37:43 +0800

    clk 刷流量器

--- src/others/clk.py
+++ src/others/clk.py
@@ -6,32 +6,107 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
-
-driver = webdriver.Firefox()
-driver.get('http://m.baidu.com')
-driver.implicitly_wait(30)
-
-driver.find_element_by_id('index-kw').send_keys('python',Keys.ENTER)
 
 def clk(driver, url):
+    """locate and click url"""
+    locator = (By.XPATH, '//span[contains(text(), "{0}")]/../../../../a'.format(url))
     try:
-        WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.XPATH, '//span[text()="{0}"]/../../a'.format(url))))
+        WebDriverWait(driver, 5, 0.5).until(EC.element_to_be_clickable(locator))
         try:
-            driver.find_element_by_xpath('//span[text()="{0}"]/../../a'.format(url)).click()
+            moved_to_element = driver.find_element_by_xpath('//span[contains(text(), "{0}")]'.format(url))
+            target_element = driver.find_element(*locator)
+            ActionChains(driver).move_to_element(to_element=moved_to_element).click(target_element).perform()
+            from time import sleep
+            sleep(1)
+            if 'm.baidu.com' in driver.current_url:
+                target_element.click()
             return 1
         except:
             return 0
     except TimeoutException:
         return 0
 
+import time
+import thread
+from threading import Thread
+from src.utils.config import DefaultConfig
+driver_path = DefaultConfig().driver_path
 
-clk(driver, url='www.python.org')
+# class BrowserTread(Thread):
+#     def __init__(self, name, n):
+#         Thread.__init__(self,name=name)
+#         self.n = n
 
-from time import sleep
+def firefox(n):
+    for i in range(n):
+        driver1 = webdriver.Firefox()
+        driver1.get('http://m.baidu.com')
+        driver1.find_element_by_id('index-kw').send_keys(u'selenium 文件上传 灰蓝', Keys.ENTER)
+        print clk(driver1, url='csdn')
+        driver1.close()
+    # thread.exit_thread()
 
-sleep(3)
+def chrome(n):
+    for i in range(n):
+        driver = webdriver.Chrome(executable_path=driver_path + 'chromedriver.exe')
+        driver.get('http://m.baidu.com')
+        driver.find_element_by_id('index-kw').send_keys(u'selenium 文件上传 灰蓝', Keys.ENTER)
+        print clk(driver, url='csdn')
+        driver.close()
+    # thread.exit_thread()
 
-print driver.current_url
+def ie(n):
+    for i in range(n):
+        driver = webdriver.Chrome(executable_path=driver_path + 'IEDriverServer.exe')
+        driver.get('http://m.baidu.com')
+        driver.find_element_by_id('index-kw').send_keys(u'selenium 文件上传 灰蓝', Keys.ENTER)
+        print clk(driver, url='csdn')
+        driver.close()
+        # thread.exit_thread()
 
-driver.close()
+
+
+if __name__ == '__main__':
+    # import threading
+    # for i in range(50):
+    #     threading.
+    #     driver1 = webdriver.Firefox()
+    #     driver1.get('http://m.baidu.com')
+    #     driver1.find_element_by_id('index-kw').send_keys(u'selenium 文件上传 灰蓝', Keys.ENTER)
+    #
+    #     print clk(driver1, url='csdn')
+    #
+    #     # sleep(3)
+    #     # print driver.current_url
+    #
+    #     driver1.close()
+    # threads = []
+    # thread1 = BrowserTread('firefox', 50)
+    # thread1.firefox()
+    # # thread1.join()
+    #
+    # thread2 = BrowserTread('chrome', 50)
+    # thread2.chrome()
+    # # thread2.join()
+    #
+    # thread3 = BrowserTread('ie', 50)
+    # thread3.ie()
+    # thread3.join()
+
+    # t1 = BrowserTread
+
+    threads = []
+    t1 = Thread(target=firefox, args=(100,))
+    threads.append(t1)
+    t2 = Thread(target=chrome, args=(200,))
+    threads.append(t2)
+    # t3 = Thread(target=ie, args=(50,))
+    # threads.append(t3)
+
+    for t in threads:
+        t.setDaemon(False)
+        t.start()
+
