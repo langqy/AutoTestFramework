05fe17ce83bac35583d985b5d948fe838c3bbafb
huilansame <396214358@qq.com>
2016-09-14 17:34:15 +0800

    修改

--- src/others/BlogHitsRobot.py
+++ src/others/BlogHitsRobot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
@@ -12,6 +12,11 @@ from threading import Thread
 from src.utils.config import DefaultConfig
 import random
 driver_path = DefaultConfig().driver_path
+KEYWORDS = [u'selenium 兼容 灰蓝', u'selenium 时间 灰蓝', u'selenium 文件上传 灰蓝', u'selenium 坑 灰蓝',
+            u'selenium editor 灰蓝', u'selenium 结构设计 灰蓝', u'selenium ActionChains 灰蓝', u'selenium alert 灰蓝',
+            u'selenium checkbox 灰蓝', u'selenium Select 灰蓝', u'selenium 网页内嵌 灰蓝', u'selenium 映射表 灰蓝',
+            u'selenium 导航栏 灰蓝', u'selenium active_element 灰蓝', u'selenium close 灰蓝', u'selenium Keys 灰蓝',
+            u'selenium 输出报告 示例 灰蓝', u'selenium autoit命令行 灰蓝']
 
 
 def clk(driver, url):
@@ -32,50 +37,6 @@ def clk(driver, url):
     except TimeoutException:
         return 0
 
-KEYWORDS = [u'selenium 兼容 灰蓝', u'selenium 时间 灰蓝', u'selenium 文件上传 灰蓝', u'selenium 坑 灰蓝',
-            u'selenium editor 灰蓝', u'selenium 结构设计 灰蓝', u'selenium ActionChains 灰蓝', u'selenium alert 灰蓝',
-            u'selenium checkbox 灰蓝', u'selenium Select 灰蓝', u'selenium 网页内嵌 灰蓝', u'selenium 映射表 灰蓝',
-            u'selenium 导航栏 灰蓝', u'selenium active_element 灰蓝', u'selenium close 灰蓝', u'selenium Keys 灰蓝',
-            u'selenium 输出报告 示例 灰蓝', u'selenium autoit命令行 灰蓝']
-
-
-def firefox(n):
-    for i in range(n):
-        driver = webdriver.Firefox()
-        driver.get('http://m.baidu.com')
-        driver.find_element_by_id('index-kw').send_keys(random.choice(KEYWORDS), Keys.ENTER)
-        print clk(driver, url='csdn')
-        driver.close()
-        sleep(1)
-
-
-def chrome(n):
-    for i in range(n):
-        driver = webdriver.Chrome(executable_path=driver_path + 'chromedriver.exe')
-        driver.get('http://m.baidu.com')
-        driver.find_element_by_id('index-kw').send_keys(random.choice(KEYWORDS), Keys.ENTER)
-        print clk(driver, url='csdn')
-        driver.close()
-        sleep(1)
-
-
-def ie(n):
-    for i in range(n):
-        driver = webdriver.Ie(executable_path=driver_path + 'IEDriverServer.exe')
-        driver.get('http://m.baidu.com')
-        driver.find_element_by_id('index-kw').send_keys(random.choice(KEYWORDS), Keys.ENTER)
-        clk(driver, url='csdn')
-        driver.close()
-
-
-def phantomjs(n):
-    for i in range(n):
-        driver = webdriver.PhantomJS(executable_path=driver_path + 'IEDriverServer.exe')
-        driver.get('http://m.baidu.com')
-        driver.find_element_by_id('index-kw').send_keys(random.choice(KEYWORDS), Keys.ENTER)
-        clk(driver, url='csdn')
-        driver.close()
-
 
 def flush(browser, n):
     for i in range(n):
@@ -93,23 +54,14 @@ def flush(browser, n):
         sleep(1)
         print driver.find_element_by_class_name('article_t').text,
         print driver.find_element_by_xpath('//p[@class="date"]/i[2]').text
-        # sleep(1)
         driver.close()
-        # driver.quit()
-        sleep(1)
+
+
+def killphantomjs():
+    os.system('taskkill /f /im phantomjs.exe')
 
 
 if __name__ == '__main__':
-    # for i in range(50):
-    #     driver1 = webdriver.Firefox()
-    #     driver1.get('http://m.baidu.com')
-    #     driver1.find_element_by_id('index-kw').send_keys(u'selenium 文件上传 灰蓝', Keys.ENTER)
-    #
-    #     print clk(driver1, url='csdn')
-    #
-    #     # sleep(3)
-    #     # print driver.current_url
-
     threads = []
     t1 = Thread(target=flush, args=('phantomjs', 20))
     threads.append(t1)
@@ -120,21 +72,8 @@ if __name__ == '__main__':
     t4 = Thread(target=flush, args=('phantomjs', 20))
     threads.append(t4)
 
-    import os
-    def killphantomjs():
-        os.system('taskkill /f /im phantomjs.exe')
-
-    # t_kill = Thread(target=killphantomjs, args=(60,))
-    # threads.append(t_kill)
-
-    # t0 = Thread()
-    # t0.start()
-
     for t in threads:
-        # t.setDaemon(False)
         t.start()
         t.join()
-
-
 
     killphantomjs()
